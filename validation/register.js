0f772d9af0d52ba86140500832e6b8c5d0d838be
Kamran Elchuzade <elchuzade@gmail.com>
2019-02-28 00:27:31 +0300

    added input registration validation options for password, email, name

--- validation/register.js
+++ validation/register.js
@@ -3,9 +3,40 @@ const isEmpty = require("./is-empty");
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
+  // here we are making sure that if the data has some value then continue
+  // but if the data is empty object or undefined or null etc then set it to empty string
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : ""; // for confirm password
+
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
   }
+  // here we check if it is an empty string then we send an error message
+  // so practically all of the empty, null, undefined are treated as empty string
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+  if (Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+  if (Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be between 6 and 30 characters";
+  }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm password field is required";
+  }
+  if (Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
+  }
+
   return {
     errors,
     isValid: isEmpty(errors)
