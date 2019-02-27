1bc265d4f36c991ee06b5cc4a0c420d69d4118bc
Kamran Elchuzade <elchuzade@gmail.com>
2019-02-27 23:52:58 +0300

    implemented a route for mypage when a user is logged in and error message if email or password is incorrect

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // @route GET api/posts/test
 // @desc Tests post route
 // @access Public
-router.get('/test', (req, res) => {
-  res.json({ msg: 'Posts works fine' });
+router.get("/test", (req, res) => {
+  res.json({ msg: "Posts works fine" });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express       = require('express');
-const mongoose      = require('mongoose');
-const bodyParser    = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const users     = require('./routes/api/users');
-const posts     = require('./routes/api/posts');
-const profile   = require('./routes/api/profile');
-
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
 
 const app = express();
 
 // Body parser middleware
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 // .connect is an action to perform
@@ -23,17 +23,19 @@ const db = require('./config/keys').mongoURI;
 // .catch means if it doesnt work fine
 mongoose
   .connect(db)
-  .then(() => console.log('MongoDB Connected'))
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-  res.send('cool website yeah');
-});
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport.js")(passport);
 
 // Use Routes
-app.use('/api/users', users);
-app.use('/api/posts', posts);
-app.use('/api/profile', profile);
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
 
 // first one is for deploying on Heroku server
 const port = process.env.PORT || 5000;
