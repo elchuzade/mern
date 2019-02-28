30ff6347f19bed363ff8c887ce9c2b22b193f613
Kamran Elchuzade <elchuzade@gmail.com>
2019-02-28 23:33:31 +0300

    added user data to the redux to stay at each upload or at any page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
+
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -10,6 +14,18 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 
 import "./App.css";
+
+// Each time we refresh the page or go to any page on the website it will check if the token has expired or not
+// if the token still exists it will set the user info to the localStorage so that we can access it at any request
+// Check for token
+if(localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 // store is to hold all the data that has been fethced from the server while surfing the website
 class App extends Component {
