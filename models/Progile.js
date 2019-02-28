d0c55645878797331966c4b50da1ad4ad5d3def7
Kamran Elchuzade <elchuzade@gmail.com>
2019-02-28 12:17:17 +0300

    added profile get request to the server side

---
+++ models/Progile.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ProfileSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "users" },
+    handle: { type: String, required: true, max: 40 },
+    company: { type: String },
+    webisite: { type: String },
+    location: { type: String },
+    status: { type: String, required: true },
+    skills: { type: [String], required: true },
+    bio: { type: String },
+    githubusername: { type: String },
+    experience: [
+      { title: { type: String, required: true } },
+      { company: { type: String, required: true } },
+      { location: { type: String } },
+      { from: { type: Date, required: true } },
+      { to: { type: Date } }, // not required coz it could be till now
+      { current: { type: Boolean, default: false } }, // if its not triggered by user it will be set default
+      { description: { type: String } }
+    ],
+    education: [
+      { school: { type: String, required: true } },
+      { degree: { type: String, required: true } },
+      { filedofstudy: { type: String, required: true } },
+      { from: { type: Date, required: true } },
+      { to: { type: Date } }, // not required coz it could be till now
+      { current: { type: Boolean, default: false } }, // if its not triggered by user it will be set default
+      { description: { type: String } }
+    ],
+    social: {
+      youtube: { type: String },
+      twitter: { type: String },
+      facebook: { type: String },
+      linkedin: { type: String },
+      instagram: { type: String }
+    },
+    date: { type: Date, default: Date.now() }
+  },
+  { timestamps: true }
+);
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
