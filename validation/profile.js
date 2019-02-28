eaac79d2f6f1b7f269fa733938259fdf6496d9ce
Kamran Elchuzade <elchuzade@gmail.com>
2019-02-28 12:46:04 +0300

    added profile info fields validator for create new nad update profile routes

---
+++ validation/profile.js
@@ -0,0 +1,62 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+  // here we are making sure that if the data has some value then continue
+  // but if the data is empty object or undefined or null etc then set it to empty string
+  data.handle = !isEmpty(data.handle) ? data.handle : "";
+  data.status = !isEmpty(data.status) ? data.status : "";
+  data.skills = !isEmpty(data.skills) ? data.skills : "";
+
+  // here we check if it is an empty string then we send an error message
+  // so practically all of the empty, null, undefined are treated as empty string
+
+  if (!Validator.isLength(data.handle, { min: 2, max: 40 })) {
+    errors.handle = "Handle needs to be between 2 and 40 cahracters";
+  }
+  if (Validator.isEmpty(data.handle)) {
+    errors.handle = "Profile handle is required";
+  }
+  if (Validator.isEmpty(data.status)) {
+    errors.status = "Status field is required";
+  }
+  if (Validator.isEmpty(data.skills)) {
+    errors.skills = "Skills field is required";
+  }
+  if (!isEmpty(data.website)) {
+    if (!Validator.isURL(data.website)) {
+      errors.website = "Not a valid URL";
+    }
+  }
+  if (!isEmpty(data.youtube)) {
+    if (!Validator.isURL(data.youtube)) {
+      errors.youtube = "Not a valid URL";
+    }
+  }
+  if (!isEmpty(data.twitter)) {
+    if (!Validator.isURL(data.twitter)) {
+      errors.twitter = "Not a valid URL";
+    }
+  }
+  if (!isEmpty(data.facebook)) {
+    if (!Validator.isURL(data.facebook)) {
+      errors.facebook = "Not a valid URL";
+    }
+  }
+  if (!isEmpty(data.linkedin)) {
+    if (!Validator.isURL(data.linkedin)) {
+      errors.linkedin = "Not a valid URL";
+    }
+  }
+  if (!isEmpty(data.instagram)) {
+    if (!Validator.isURL(data.instagram)) {
+      errors.instagram = "Not a valid URL";
+    }
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
