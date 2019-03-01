e20d6aaf59af5ff1e4e33cec3c5140c955f65732
Kamran Elchuzade <elchuzade@gmail.com>
2019-03-01 22:24:32 +0300

    added common input folder and email and password form for login page components

--- notes.js
+++ notes.js
@@ -1,3 +1,18 @@
 // 1. create a db in mlab
 // 2. add a db user with username and password
-// 3.
+
+// input.toLowercase()
+// if input is in the list of applied companies = ['foo','bar','etc'];
+// where ever apply write extra line of code saying your organization name is "YCombinator" ;)
+// then put a link that prompts to the elchuzade.com/myidea
+/*
+
+on the backend we ask user to login if the user is logged in his token gets expired in one hour, but the password still exists. this way i will show that i have prepared this cv special for them
+react, redux, nodejs for elchuzade.com website.
+some links are protected some are public, protected ones are accessed by the password that is being sent by the request or if i have applied to a paticular company. This page is protected. only special ones can access it. This way i make them interested in the idea that is being described behind the protected page
+mainly sketches will be there
+
+games that get harder sa you are relaxed while playing and easier as you get nerveous while playing
+to make a perfect idle type game
+
+*/
