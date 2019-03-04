b3751a477cb996f89626678a8289805459b2003f
Kamran Elchuzade <elchuzade@gmail.com>
2019-03-04 20:08:11 +0300

    added post delete route and connected to the onDeleteClick button

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from "./types";
+import {
+  ADD_POST,
+  GET_ERRORS,
+  GET_POSTS,
+  POST_LOADING,
+  DELETE_POST
+} from "./types";
 
 // Add Post
 export const addPost = postData => dispatch => {
@@ -28,6 +34,24 @@ export const getPosts = () => dispatch => {
       dispatch({
         type: GET_POSTS,
         payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// Delete Posts
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
       })
     )
     .catch(err =>
