e94cdd3e89f333fcdfbc2c3c8ecae992b4300ed3
Kamran Elchuzade <elchuzade@gmail.com>
2019-03-04 20:10:22 +0300

    added addLike and removeLike actions

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -25,6 +25,32 @@ export const addPost = postData => dispatch => {
     );
 };
 
+// Add Like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Remove Like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // GET Posts
 export const getPosts = () => dispatch => {
   dispatch(setLPostLoading());
